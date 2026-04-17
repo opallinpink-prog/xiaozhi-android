801ec99d26903acd1f10f88cfc64b8a08cc7a18f
opallinpink-prog <opallinpink@gmail.com>
2026-04-17 18:48:24 +0200

    Update MainActivity.kt

--- app/src/main/java/info/dourok/voicebot/MainActivity.kt
+++ app/src/main/java/info/dourok/voicebot/MainActivity.kt
@@ -2,7 +2,7 @@ package info.dourok.voicebot
 
 import android.Manifest
 import android.app.Activity
-import android.content.content.pm.PackageManager
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
