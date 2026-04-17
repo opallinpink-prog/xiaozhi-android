2c400d8a9e91232aadeee99909eebd07bb5c98f6
opallinpink-prog <opallinpink@gmail.com>
2026-04-17 18:40:06 +0200

    Update MainActivity.kt

--- app/src/main/java/info/dourok/voicebot/MainActivity.kt
+++ app/src/main/java/info/dourok/voicebot/MainActivity.kt
@@ -2,68 +2,40 @@ package info.dourok.voicebot
 
 import android.Manifest
 import android.app.Activity
-import android.content.Context
-import android.content.pm.PackageManager
-import android.media.AudioManager
+import android.content.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import dagger.hilt.EntryPoint
-import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.EntryPointAccessors
-import dagger.hilt.android.components.ActivityComponent
-import info.dourok.voicebot.data.FormRepository
 import info.dourok.voicebot.ui.ActivationScreen
 import info.dourok.voicebot.ui.ChatScreen
 import info.dourok.voicebot.ui.ServerFormScreen
 import info.dourok.voicebot.ui.theme.VoicebotclientandroidTheme
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
-
+import info.dourok.voicebot.viewmodel.ChatViewModel
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        (getSystemService(AUDIO_SERVICE) as AudioManager).mode = AudioManager.MODE_IN_COMMUNICATION
         Log.d("MainActivity", "onCreate")
         if (ActivityCompat.checkSelfPermission(
                 this,
@@ -75,9 +47,8 @@ class MainActivity : ComponentActivity() {
                 arrayOf(Manifest.permission.RECORD_AUDIO),
                 0
             )
-        } else {
-            Log.d("MainActivity", "Permission granted")
         }
+        
         enableEdgeToEdge()
         setContent {
             VoicebotclientandroidTheme {
@@ -99,9 +70,6 @@ fun AppNavigation() {
     val entryPoint = EntryPointAccessors.fromActivity(activity, NavigationEntryPoint::class.java)
     val navigationEvents = entryPoint.getNavigationEvents()
 
-
-    Log.d("AppNavigation", "navigationEvents: $navigationEvents")
-
     LaunchedEffect(navController) {
         navigationEvents.collect { route ->
             navController.navigate(route)
@@ -111,7 +79,10 @@ fun AppNavigation() {
     NavHost(navController = navController, startDestination = "form") {
         composable("form") { ServerFormScreen() }
         composable("activation") { ActivationScreen() }
-        composable("chat") { ChatScreen() }
+        composable("chat") { 
+            // Correctly passing the injected ViewModel to ChatScreen
+            val chatViewModel: ChatViewModel = hiltViewModel()
+            ChatScreen(viewModel = chatViewModel) 
+        }
     }
 }
-
